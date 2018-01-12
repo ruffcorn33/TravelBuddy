e8f831e6ac672105ceba65b13c88bbfa97f8ecca
Craig Christensen <ruffcorn@gmail.com>
2018-01-12 13:21:30 -0600

    added wireframe for all pages

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD1UZeSuXUW52NR0tDe3ZR6RX3UMR_GzhA",
+  authDomain: "travelbuddy-73a26.firebaseapp.com",
+  databaseURL: "https://travelbuddy-73a26.firebaseio.com",
+  projectId: "travelbuddy-73a26",
+  storageBucket: "travelbuddy-73a26.appspot.com",
+  messagingSenderId: "292320888078"
+};
+firebase.initializeApp(config);
