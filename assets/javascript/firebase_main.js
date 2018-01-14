d1d5b18d90c4b5c511781823dd3459256dc82611
Christopher Biessener <cpbiessener@gmail.com>
2018-01-14 12:00:06 -0600

    'fake' user is logged in

--- assets/javascript/firebase_main.js
+++ assets/javascript/firebase_main.js
@@ -12,8 +12,11 @@
 
 // array of activity categories stored in Firebase
 var activityCategoriesArray = [];
+// current user
+var user = null;
 
 // set activityCategoriesArray once at first load
+// also build the html for the select (TODO - move this loop to appropriate function)
 activity_categories_ref.orderByKey().once('value').then(function(snapshot)
 {
   activityCategoriesArray = snapshot.val();
@@ -24,6 +27,14 @@ activity_categories_ref.orderByKey().once('value').then(function(snapshot)
     var option = $('<option>' + activityCategoriesArray[i] + '</option>');
     $('#inp_category').append(option);
   }
+});
+
+// TODO - use proper log in stuff - 'child_added' is used instead of 'value' to get only one user
+users_ref.once('child_added').then(function(user_snap)
+{
+  console.log(user_snap.val())
+  user = user_snap.val();
+  login_user(user);
 });
 
 //
@@ -66,8 +77,24 @@ $("#submit_activity").on("click", function(event)
 //             + '<td>' + activity.location + '</td>'
 //             );
 //   // Writes the saved value from firebase to our display
-//   $("#additionalRows").append(tr);
+//   $("#activityRows").append(tr);
 // }, function(errorObject)
 // { // Handles firebase failure if it occurs
 //   console.log("The read failed: " + errorObject.code);
 // });
+
+//
+// Utility Functions
+//
+function login_user(user)
+{
+  console.log("logging in:", user.name, user.email);
+  // TODO - replace this placeholder code
+  // build the table row
+  var tr = $('<tr>'
+            + '<td>' + user.name + '</td>'
+            + '<td>' + user.email + '</td>'
+            );
+  // Writes the saved value from firebase to our display
+  $("#userRow").append(tr);
+}
