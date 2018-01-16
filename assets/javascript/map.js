15da6be4ace9a0b2a5d5b087b0d290b35abb9b26
Craig Christensen <ruffcorn@gmail.com>
2018-01-16 12:48:16 -0600

    Display map on main page based on input from welcome page.

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,10 +1,22 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyD1UZeSuXUW52NR0tDe3ZR6RX3UMR_GzhA",
-  authDomain: "travelbuddy-73a26.firebaseapp.com",
-  databaseURL: "https://travelbuddy-73a26.firebaseio.com",
-  projectId: "travelbuddy-73a26",
-  storageBucket: "travelbuddy-73a26.appspot.com",
-  messagingSenderId: "292320888078"
-};
-firebase.initializeApp(config);
+
+// get trip values from local storage
+var tripAddress = localStorage.getItem("tripLoc");
+var tripPid = localStorage.getItem("tripPid");
+
+$("#titleDisplay").text(tripAddress);
+
+var mapById = document.getElementById('map');
+function initMap(){
+  var tripLat = Number(localStorage.getItem("tripLat"));
+  var tripLng = Number(localStorage.getItem("tripLng"));
+  var latLng = {
+    lat:tripLat,
+    lng:tripLng,
+  };
+  console.log(latLng);
+  var options = {
+    zoom:13,
+    center:latLng
+  }
+  var map = new google.maps.Map(mapById, options);
+}
